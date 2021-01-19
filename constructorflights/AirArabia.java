6965698acb82c8a76cca0eebdd34b3233b96e068
Gul <57442171+Gul224@users.noreply.github.com>
2021-01-19 13:58:31 +0530

    Add Constructor Assignment
    In this Assignment,  Initialised Constructors and passed  values which is a  "parameterised constructor"

---
+++ constructorflights/AirArabia.java
@@ -0,0 +1,12 @@
+package constructorflights;
+
+public class AirArabia  {
+ public static void main(String[]   args){
+
+     InternationalFlights flights1 = new InternationalFlights("Dubai", 456);
+     System.out.println("Flightname :" + flights1.country + "  and FlightId: " + flights1.id );
+ }
+ }
+
+
+
