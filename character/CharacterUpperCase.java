b2c84e640cbf96bd3bc6894908e2a9edd6aacfcc
Gul <57442171+Gul224@users.noreply.github.com>
2021-01-20 12:35:27 +0530

    Added  Cryptography Assignments
    1. Arrays - Created an Array with multiple values
    2. Character - Created Character class with commonly used Methods 
    3. String Manipulation - Manipulation of String with different placeholders
    4. Palindromes - Using of backwards and forwards 
    5. Counting loops

---
+++ character/CharacterUpperCase.java
@@ -0,0 +1,15 @@
+package character;
+
+public class CharacterUpperCase {
+    public static void main(String[]  args){
+        char ch = 'g';
+        if (Character.isUpperCase(ch)){
+            System.out.println("Its in Upper case");
+        }
+        else{
+            System.out.println("No, its not in upper case");
+        }
+
+
+    }
+}
